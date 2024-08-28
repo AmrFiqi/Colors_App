0527372a9993275394e1f4f1b6cfe2957ddaa2c8
AmrFiqi <amr.h.elfiqi@gmail.com>
2024-08-28 16:17:40 +0300

    Setup TableViewController & created a function to generate random UIColor

--- Colors App/ColorsTableVC.swift
+++ Colors App/ColorsTableVC.swift
@@ -8,18 +8,45 @@
 import UIKit
 
 class ColorsTableVC: UIViewController {
-
+    
+    // MARK: - Variables
+    
+    var colors: [UIColor] = []
+    
     
     // MARK: - Functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func tempButtonTapped(_ sender: Any) {
-        performSegue(withIdentifier: "ToColorsDetailVC", sender: nil)
+    func createRandomColors() {
+        for _ in 0..<50 {
+            colors.append(createRandomColor())
+        }
     }
     
+    func createRandomColor() -> UIColor {
+        let randomColor = UIColor(red: CGFloat.random(in: 0...1.0), green: CGFloat.random(in: 0...1.0), blue: CGFloat.random(in: 0...1.0), alpha: 0)
+        return randomColor
+    }
+    
+}
 
+// MARK: - Extension
+
+extension ColorsTableVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ToColorsDetailVC", sender: nil)
+    }
 }
